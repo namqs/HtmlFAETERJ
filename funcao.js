ceda46cd28464b5ba8a2e88cad7b102d62fa2ec1
Natalie Marques <namqsf@gmail.com>
2022-11-27 20:56:32 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Valeu ' + nome.value + ', já já lerei a sua mensagem! :)');
+    }
+
+}
